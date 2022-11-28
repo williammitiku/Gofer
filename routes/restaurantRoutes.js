d76bf55676d3a65cc32ee6118738e335783eb2e2
William Mitiku <williammitiku95@gmail.com>
2022-11-28 06:30:54 -0800

    wup

--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -9,9 +9,12 @@ const authAdmins = require('../middleware/authAdmins')
 
 router.post("/create", authA, authAdmins, restaurantController.create)
 router.post("/register", authA, authAdmins, restaurantController.register)
-router.post("/getAll", auth, restaurantController.getAll)
+router.get("/getAll", auth, restaurantController.getAll)
+router.get("/getAllRestaurants", authA, authAdmins, restaurantController.getAll)
 router.post("/delete", authA, authAdmins, restaurantController.delete)
 router.post("/update", authA, authAdmins, restaurantController.update)
+router.post("/getNearyBy", restaurantController.getNearyBy)
+
 
 module.exports = router;
 
